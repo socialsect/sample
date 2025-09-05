680b8895c46084d3c0ebc4d3fba7f207d0b55772
Socialsect <rayanshofficial@gmail.com>
2025-09-06 04:10:25 +0530

    Update methodologies.jsx

--- src/pages/methodologies/methodologies.jsx
+++ src/pages/methodologies/methodologies.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import './Methodologies.css';
+import './methodologies.css';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer/Footer';
 const FerzMethodologies = () => {
